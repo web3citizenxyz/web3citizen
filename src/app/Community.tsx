04205e5dcb83604c0f66cbd36c3ade6ccb98b7a2
Javier Oviedo <javieroviedo@Air-de-Javier.lan>
2024-07-05 14:13:37 +0100

    spaces

--- src/app/Community.tsx
+++ src/app/Community.tsx
@@ -12,7 +12,10 @@ export default function Community () {
     <h1>Community</h1>
     <p>Stay Updated with the Latest Trends</p>
     <p>Stay informed about the latest projects and DAOs in the Ethereum ecosystem.</p>
-    <a href='https://t.me/web3citizenxyznews' target='_blank' className={styles.btn}>Join Our Community</a>
+    
+    <button className={styles.btn}>
+        <a href='https://t.me/web3citizenxyznews' target='_blank'>Join Our Community</a>
+        </button>
   </div>
   <div className={styles.svgcommunity}>
         <img src="/images/community-elipse.svg" alt="Semicircles" className={styles.communityelipse}/>
--- src/app/Team.tsx
+++ src/app/Team.tsx
@@ -11,8 +11,9 @@ export default function Team() {
     <h1>Contributors</h1>
     <p>Join Our Open Source Community</p>
     <p>All our content is open source. We aim to create a collaborative research environment where you can share knowledge and explore different areas of the Ethereum ecosystem.</p>
-  
-    <a href='https://github.com/web3citizenxyz/web3citizen' target='_blank' className={styles.btn}>Start Contributing</a>
+<button className={styles.btn}>
+    <a href='https://github.com/web3citizenxyz/web3citizen' target='_blank'>Start Contributing</a>
+    </button>
   </div>
   <div className={styles.teamsvg}>
         <img src="/images/elipse-team.svg" alt="Semicircles" className={styles.elipseTeam}/>
