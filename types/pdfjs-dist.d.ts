17d93bf7c54cf35a00757c50400696060e0ab62d
Javier Oviedo <javieroviedo@172-15-172-84.lightspeed.mssnks.sbcglobal.net>
2024-07-05 12:59:19 +0100

    Add custom type declarations for pdfjs-dist

---
+++ types/pdfjs-dist.d.ts
@@ -0,0 +1,9 @@
+declare module 'pdfjs-dist/build/pdf' {
+    const pdfjsLib: any;
+    export = pdfjsLib;
+}
+
+declare module 'pdfjs-dist/build/pdf.worker.entry' {
+    const worker: any;
+    export = worker;
+}
