bbc6b1bef42b6d26d7d51ac4a4735b274a89219f
Javier Oviedo <javieroviedo@Air-de-Javier.lan>
2024-07-05 09:46:11 +0100

    share

--- src/components/SideIcons.tsx
+++ src/components/SideIcons.tsx
@@ -6,10 +6,13 @@ const SideIcons: React.FC = () => {
     return (
         <div className={styles.sideIcons}>
             <div className={styles.iconContainer}>
+            <a href="" download>
                 <img src="/icons/guardar-article.svg" alt="Back to top" className={styles.icon} onClick={() => window.scrollTo({ top: 0, behavior: 'smooth' })} />
+          </a>
             </div>
             <div className={styles.iconContainer}>
-                <a href="" download>
+               
+                <a href='https://x.com/web3citizenxyz?s=21' target='_blank'> 
                     <img src="/icons/descargar-article.svg" alt="Download PDF" className={styles.icon} />
                 </a>
             </div>
