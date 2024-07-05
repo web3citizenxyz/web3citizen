ae2a8d64df8b50ba4c3aa03c7d4e0a77eebe9bb0
Javier Oviedo <javieroviedo@Air-de-Javier.lan>
2024-07-05 08:31:48 +0100

    changes

--- src/app/Team.tsx
+++ src/app/Team.tsx
@@ -5,16 +5,24 @@ import styles from './styles/Team.module.css';
 
 export default function Team() {
   return (
-    <section className={styles.team}>
-      <div className={styles.content}>
-        <h1>Contributors</h1>
-        <p>Join Our Open Source Community</p>
-        <p>All our content is open source. We aim to create a collaborative research environment where you can share knowledge and explore different areas of the Ethereum ecosystem.</p>
-        <a href='https://github.com/web3citizenxyz/web3citizen' target='_blank' className={styles.btn}>Start Contributing</a>
-      </div>
 
-      <div className={styles.carousel}>
-        {/*
+<section className={styles.team}>
+  <div className={styles.content}>
+    <h1>Contributors</h1>
+    <p>Join Our Open Source Community</p>
+    <p>All our content is open source. We aim to create a collaborative research environment where you can share knowledge and explore different areas of the Ethereum ecosystem.</p>
+  
+    <a href='https://github.com/web3citizenxyz/web3citizen' target='_blank' className={styles.btn}>Start Contributing</a>
+  </div>
+  <div className={styles.teamsvg}>
+        <img src="/images/elipse-team.svg" alt="Semicircles" className={styles.elipseTeam}/>
+      </div>
+</section>
+
+
+);
+}
+  {/*
         <div className={styles.carouselWrapper}>
           <div className={`${styles.slide} ${styles.first}`}>
             <img src="/images/profile-2.png" alt="Contributor 1" className={styles.contributorImage} />
@@ -26,9 +34,3 @@ export default function Team() {
             <img src="/images/profile-3.jpeg" alt="Contributor 3" className={styles.contributorImage} />
           </div> 
   </div> */}
-        <img src="/images/elipse-team.svg" alt="Semicircles" className={styles.elipseTeam} />
-      </div>
-
-    </section>
-  );
-}
