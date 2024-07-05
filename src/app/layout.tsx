89be460792b9f9a4894b476217e851a2f38b4911
Javier Oviedo <javieroviedo@Air-de-Javier.lan>
2024-07-05 16:26:44 +0100

    graphs

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,10 +1,11 @@
 "use client";
-
+import { ReactNode, useState, useEffect, useRef} from 'react';
+import Head from 'next/head';
 
 import Link from 'next/link';
-import { ReactNode, useState, useEffect, useRef} from 'react';
+
 import { usePathname } from 'next/navigation';
-import Head from 'next/head';
+
 import './styles/globals.css'; 
 import styles from './styles/Layout.module.css';
 import Intro from './intro/Intro';
@@ -109,49 +110,39 @@ export default function RootLayout({ children }: LayoutProps) {
   }, [pathname]);
 
 
-
   return (
-    <html lang="en">
-    
-    <Head>
-    <title>Web3 Citizen</title>
-        <meta name="description" content="Your infinite garden guide" />
-        
-        {/* Favicons */}
-        <link rel="icon" href="/favicon.ico" type="image/x-icon" />
-        <link rel="icon" href="/favicon.svg" type="image/svg+xml" />
-        <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
-        <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
-        <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
-        <link rel="manifest" href="/site.webmanifest" />
-        <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#5bbad5" />
-        <meta name="msapplication-TileColor" content="#da532c" />
-        <meta name="theme-color" content="#ffffff" />
-
-        {/* Fonts */}
-        <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Montserrat:wght@400;500;700&family=Roboto+Mono:wght@300;400;700&display=swap" />
-
-        {/* Open Graph tags */}
-        <meta property="og:title" content="Web3 Citizen - Your infinite garden guide" />
-        <meta property="og:description" content="Explore our curated resources and tutorials to understand the fundamentals of the decentralized web." />
-        <meta property="og:image" content="https://www.web3citizen.xyz/images/0g.png" />
-        <meta property="og:url" content="https://www.web3citizen.xyz/" />
-        <meta property="og:type" content="website" />
-        <meta property="og:site_name" content="Web3 Citizen" />
-
-        {/* Google Analytics */}
-        <script async src="https://www.googletagmanager.com/gtag/js?id=G-2XB363WRVJ"></script>
-        <script
-          dangerouslySetInnerHTML={{
-            __html: `
-              window.dataLayer = window.dataLayer || [];
-              function gtag(){dataLayer.push(arguments);}
-              gtag('js', new Date());
-              gtag('config', 'G-2XB363WRVJ');
-            `,
-          }}
-        />
-      </Head>
+  <html lang="en">
+    <head>
+        <title>Web3 Citizen</title>
+          <meta name="description" content="Your infinite garden guide" />
+          <link rel="icon" href="/favicon.ico" type="image/x-icon" />
+          <link rel="icon" href="/favicon.svg" type="image/svg+xml" />
+          <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+          <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+          <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+          <link rel="manifest" href="/site.webmanifest" />
+          <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#5bbad5" />
+          <meta name="msapplication-TileColor" content="#da532c" />
+          <meta name="theme-color" content="#ffffff" />
+          <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Montserrat:wght@400;500;700&family=Roboto+Mono:wght@300;400;700&display=swap" />
+          <meta property="og:title" content="Web3 Citizen - Your infinite garden guide" />
+          <meta property="og:description" content="Explore our curated resources and tutorials to understand the fundamentals of the decentralized web." />
+          <meta property="og:image" content="https://www.web3citizen.xyz/images/0g.png" />
+          <meta property="og:url" content="https://www.web3citizen.xyz/" />
+          <meta property="og:type" content="website" />
+          <meta property="og:site_name" content="Web3 Citizen" />
+          <script async src="https://www.googletagmanager.com/gtag/js?id=G-2XB363WRVJ"></script>
+          <script
+            dangerouslySetInnerHTML={{
+              __html: `
+                window.dataLayer = window.dataLayer || [];
+                function gtag(){dataLayer.push(arguments);}
+                gtag('js', new Date());
+                gtag('config', 'G-2XB363WRVJ');
+              `,
+            }}
+          />
+</head>
       <body>
       {!introFinished && pathname === '/' && <Intro onFinish={() => setIntroFinished(true)} />}
       <div className={`${styles.container}`}>
