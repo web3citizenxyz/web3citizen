c1411224ebc89da706112d43ec23ca6b8ec8a5ae
Javier Oviedo <javieroviedo@MacBook-Air-de-Javier.local>
2024-07-19 19:09:38 +0100

    home

--- src/app/Team.tsx
+++ src/app/Team.tsx
@@ -1,37 +1,50 @@
 "use client";
 
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import styles from './styles/Team.module.css';
 
 export default function Team() {
+  const [currentIndex, setCurrentIndex] = useState(0);
+  const items = [
+    "/images/profile-1.jpg",
+    "/images/profile-2.png",
+    "/images/profile-3.jpeg",
+    "/images/profile-1.jpg",
+    "/images/profile-2.png",
+    "/images/profile-3.jpeg",
+  ];
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setCurrentIndex((prevIndex) => (prevIndex + 1) % items.length);
+    }, 3000); 
+
+    return () => clearInterval(interval);
+  }, [items.length]);
+
   return (
-
-<section className={styles.team}>
-  <div className={styles.content}>
-    <h1>Contributors</h1>
-    <p>Join Our Open Source Community</p>
-    <p>All our content is open source. We aim to create a collaborative research environment where you can share knowledge and explore different areas of the Ethereum ecosystem.</p>
-<button className={styles.btn}>
-    <a href='https://github.com/web3citizenxyz/web3citizen' target='_blank'>Start Contributing</a>
-    </button>
-  </div>
-  <div className={styles.teamsvg}>
-        <img src="/images/elipse-team.svg" alt="Semicircles" className={styles.elipseTeam}/>
+    <section className={styles.team}>
+      <div className={styles.content}>
+        <h1>Contributors</h1>
+        <p>Join Our Open Source Community</p>
+        <p>All our content is open source. We aim to create a collaborative research environment where you can share knowledge and explore different areas of the Ethereum ecosystem.</p>
+        <button className={styles.btn}>
+          <a href='https://github.com/web3citizenxyz/web3citizen' target='_blank'>Start Contributing</a>
+        </button>
       </div>
-</section>
-
-
-);
+      <div className={styles.teamsvg}>
+        <div className={styles.carousel}>
+          {items.map((item, index) => (
+            <div
+              key={index}
+              className={`${styles.carouselItem} ${index === currentIndex ? styles.active : ''}`}
+            >
+              <img src={item} alt={`User ${index + 1}`} />
+            </div>
+          ))}
+        </div>
+        <img src="/images/elipse-team.svg" alt="Semicircles" className={styles.elipseTeam} />
+      </div>
+    </section>
+  );
 }
-  {/*
-        <div className={styles.carouselWrapper}>
-          <div className={`${styles.slide} ${styles.first}`}>
-            <img src="/images/profile-2.png" alt="Contributor 1" className={styles.contributorImage} />
-          </div>
-          <div className={`${styles.slide} ${styles.active}`}>
-            <img src="/images/profile-1.jpg" alt="Contributor 2" className={styles.contributorImage} />
-          </div>
-          <div className={`${styles.slide} ${styles.third}`}>
-            <img src="/images/profile-3.jpeg" alt="Contributor 3" className={styles.contributorImage} />
-          </div> 
-  </div> */}
