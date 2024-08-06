8edb944f33aa2cfbeca2aa96f51d6a45dedef847
Javier Oviedo <javieroviedo@192.168.1.17>
2024-08-06 15:17:47 -0300

    blueprint

--- src/app/research/grants/aleo-blueprint/page.tsx
+++ src/app/research/grants/aleo-blueprint/page.tsx
@@ -17,8 +17,12 @@ export default function AleoBluePrint() {
       <section className={styles.section}>
         <h2>Aleo Blueprint Grants</h2>
       
-        <p><strong>Timeframe: </strong> Rolling Basis</p>
-        <p><strong>Amount:</strong> 10K-100K</p>
+        <br></br>
+      <img src="/images/aleo-blue.svg" alt="Aleo" className={styles.benefitsImage} />
+
+   <br></br>
+<br></br>
+<h1> What are Blueprint Grants?</h1>   
 <p> 
 Blueprint Grants are awarded to build pre-specified projects as identified by Aleo - also known as Requests for Proposals (RFPs). These grants aim to fund key components or products on the Aleo network.
 </p> 
@@ -26,22 +30,27 @@ Blueprint Grants are awarded to build pre-specified projects as identified by Al
   </section>
 
  
+ <br></br>
   <section className={styles.section}>
     
-   <h1>Grant Scope</h1>   
+   <h1>Scope</h1>   
   <p>Blueprint Grants award prospective projects oriented explicitly towards developers and builders.</p>
   <p>The scope of Blueprint Grant varies according to specific needs in the ecosystem. Needs include but are not limited to designing bounty reward systems or developing projects in Zero-Knowledge Decentralized Finance (ZeFi).</p>
   <p>Explore all open <a className={styles.a} href='https://www.notion.so/Aleo-Blueprint-Grants-dba7966ddc95434ba8b90a2306485494?pvs=4'>Blueprint Grants.</a></p>
    
    
 </section>
-
-<h1>Funding Specifics</h1>
+<br></br>
+<section className={styles.section}>
+    
+<h1>Funding</h1>
 <p>The funding amount is disbursed based on the deliverance of milestones and goals of the submitted solution.</p>
 <p>The funding amount and payment currency vary for each Blueprint Grant depending on the complexity and characteristics of the component.</p>
 
+</section>
+<br></br>
 
-
+<section className={styles.section}>
 <h1>Requirements</h1>
 <ul className={styles.list}>
 <li>Each Blueprint Grant has its own identified need or problem, design specifications, milestones, deliverables, and requirements.</li>
@@ -49,21 +58,25 @@ Blueprint Grants are awarded to build pre-specified projects as identified by Al
 
 </ul>
 
+</section>
+<br></br>
 
-
+<section className={styles.section}>
 <h1>Grant Process</h1>
-<ul className={styles.list}>
-  <p><strong>Application:</strong></p>
-<li>1. Read through the <a className={styles.a} href='https://www.notion.so/Aleo-Blueprint-Grants-dba7966ddc95434ba8b90a2306485494?pvs=4'>available grants.</a> Submit your <a href='https://airtable.com/appT1RrkReCRoTlP4/shrI5cYn5w3hkfUvY' className={styles.a}>application form</a> selecting the Blueprint for which you are applying.</li>
-<p><strong>Review</strong></p>
 
- <li>1. Proposals are evaluated by a Grant Committee on a rolling basis according to their adherence to design specifications, their ability to satisfy the impact requirements, feasibility, innovation, and proposed solution.</li>
- <li>2. After the selection process is complete, the chosen projects are contacted.</li>
+<ul className={styles.list}>
+ <p className={styles.subtitle}><strong>Application:</strong></p>
+<p>1. Read through the <a className={styles.a} href='https://www.notion.so/Aleo-Blueprint-Grants-dba7966ddc95434ba8b90a2306485494?pvs=4'>available grants.</a> Submit your <a href='https://airtable.com/appT1RrkReCRoTlP4/shrI5cYn5w3hkfUvY' className={styles.a}>application form</a> selecting the Blueprint you wish to apply to.</p>
+
+<p className={styles.subtitle}><strong>Review</strong></p>
+
+ <p>1. Proposals are evaluated by a Grant Committee on a rolling basis according to their adherence to design specifications, their ability to satisfy the impact requirements, feasibility, innovation, and proposed solution.</p>
+ <p>2. Selected projects are contacted.</p>
  </ul> 
 
+</section>
 
-
-
+<br></br>
 
 <h1>Resources</h1>
       <ul className={styles.list}>
--- src/app/research/grants/aleo-launch/page.tsx
+++ src/app/research/grants/aleo-launch/page.tsx
@@ -9,35 +9,56 @@ import BackToTop from '@/components/BackToTop';
 
 
 
-export default function AleoBluePrint() {
+export default function AleoLaunch() {
   return (
     <div className={styles.articleContainer}>
   
       
       <section className={styles.section}>
         <h2>Aleo Launch Grants</h2>
-      
-        <p><strong>Timeframe: </strong> Rolling Basis</p>
-        <p><strong>Amount:</strong> 10K-200K+</p>
-<p> Launch Grants aims to provide funding for comprehensive, full-featured projects with multiple milestones built on the Aleo network.</p> 
-<p>Launch Grants exist within Aleo’s more extensive Developer Grants Program. Across three categories, Aleo’s Developer Grants Program has awarded more than 15 developer projects with $1M+.</p>
+      <br></br>
+        <img src="/images/aleo-launch.svg" alt="Aleo" className={styles.benefitsImage} />
+<br></br>
+<br></br>
+<h1>What are Launch Grants?</h1>
+<p>
+Launch Grants provide funding for comprehensive, full-featured projects with multiple milestones built on the Aleo network.
+</p> 
+<p>Launch Grants exist within Aleo’s more extensive Developer Grants Program. Across three categories, Aleo’s Developer Grants Program has awarded developer projects with $1M+.</p>
   </section>
 
- 
+ <br></br>
+
+
   <section className={styles.section}>
     
-   <h1>Grant Scope</h1>   
-  <p>All project ideas are welcome as long as they fulfill grant requirements and benefit the Aleo ecosystem. Aleo identifies high-value categories: gaming, Identity & Authentication, Zero-Knowledge Decentralized Finance (ZeFi), Zero-Knowledge Machine Learning, voting and governance, Social Finance, and Tooling & Infrastructure.</p>
+   <h1>Scope</h1>   
+  <p>All project ideas are welcome as long as they fulfill grant requirements and benefit the Aleo ecosystem. Aleo identifies high-value categories: </p>
+  <ul className={styles.list}>
+    <li>Gaming</li>
+    <li>Identity & Authentication</li>
+    <li>Zero-Knowledge Decentralized Finance (ZeFi)</li>
+    <li>Zero-Knowledge Machine Learning (zkML)</li>
+    <li>Voting and governance</li>
+    <li>Social Finance</li>
+    <li>Tooling & Infrastructure</li>
+
+  </ul>
   <p>In contrast to Blueprint Grants, for Launch Grants, ideation, milestone planning, and deliverables are defined and submitted by applicants. The milestones and funding amount for each milestone should depend on the project’s complexity.</p>   
    
 </section>
 
-<h1>Funding Specifics</h1>
-<p>The funding amount is disbursed based on the deliverance of milestones and goals of the submitted solution.</p>
-<p>The funding amount and payment currency vary for each Blueprint Grant depending on the complexity and characteristics of the component.</p>
+<br></br>
+
+<section className={styles.section}>
+<h1>Funding</h1>
+<p>The funding amount is disbursed based on the deliverance of milestones and goals of the submitted solution. The funding amount my vary for each Grant depending on the complexity.</p>
+
+</section>
+<br></br>
 
 
-
+<section className={styles.section}>
 <h1>Grant Requirements</h1>
 <ul className={styles.list}>
 <li>Developer projects must contain open-source elements that are built on the Aleo network.</li>
@@ -45,22 +66,30 @@ export default function AleoBluePrint() {
 <li>Applicants must submit a project overview, their team’s background information, and a grant proposal.</li>
 
 </ul>
+</section>
+<br></br>
 
+
+
+<section className={styles.section}>
 
 
 <h1>Grant Process</h1>
 <ul className={styles.list}>
-  <p><strong>Application:</strong></p>
-<li>1. Outline your proposal using the <a className={styles.a} href='https://docs.google.com/document/d/1x-9xSvPQgF_hmb_xd3e3_2czauwxrGDIXDSj_3x-JuE/edit'>.</a></li>
-<li>2. Complete the <a className={styles.a} href='https://docs.google.com/forms/d/e/1FAIpQLSeFw2SuNvYxIItIShgiibKn1iUF7xn7g_xTESGIg4HcL3NA2g/viewform'>Launch Grant Application</a> and submit the proposal.</li>
-<p><strong>Review</strong></p>
 
- <li>1. 1. Projects will be reviewed by the Grants Committee taking into consideration the selection criteria: team quality and experience (30%), project uniqueness or scope of work including well-defined applications, its features functionalities, milestone planning and feasibility (20%), and potential impact on the Aleo ecosystem (50%).</li>
- <li>2. Selected applicants must pass a KYC screening.</li>
- <li>3. After the selection process is complete, the chosen projects are contacted.</li>
+<p className={styles.subtitle}><strong>Application:</strong></p>
+<p>1. Outline your proposal using the <a className={styles.a} href='https://docs.google.com/document/d/1x-9xSvPQgF_hmb_xd3e3_2czauwxrGDIXDSj_3x-JuE/edit'>Launch Grants Template.</a></p>
+<p>2. Complete the Launch Grant Application and submit the proposal.</p>
+
+
+<p className={styles.subtitle}><strong>Review</strong></p>
+
+ <p>1. Projects will be reviewed by the Grants Committee taking into consideration the selection criteria: team quality and experience (30%), project uniqueness or scope of work including well-defined applications, its features functionalities, milestone planning and feasibility (20%), and potential impact on the Aleo ecosystem (50%).</p>
+ <p>2. Selected applicants must pass a KYC screening.</p>
+ <p>3. After the selection process is complete, the chosen projects are contacted.</p>
  </ul> 
-
-
+</section>
+<br></br>
 
 
 
