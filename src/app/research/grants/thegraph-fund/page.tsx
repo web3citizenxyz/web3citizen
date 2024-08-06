eff72f18ca8866dcd4f925587e9a33666553f1b4
Javier Oviedo <javieroviedo@192.168.1.17>
2024-08-06 13:09:10 -0300

    delete css

--- src/app/research/grants/thegraph-fund/page.tsx
+++ src/app/research/grants/thegraph-fund/page.tsx
@@ -1,6 +1,6 @@
 "use client";
 import React from 'react';
-import styles from './DaoBasics.module.css';
+import styles from '../DaoBasics.module.css';
 import SideIcons from '@/components/SideIcons';
 import BackToTop from '@/components/BackToTop';
 
