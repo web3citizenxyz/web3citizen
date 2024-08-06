8d688067a707b5792f910167de148ee7ead6db20
Javier Oviedo <javieroviedo@192.168.1.17>
2024-08-06 17:02:38 -0300

    decentraland

--- src/app/research/grants/decentraland/page.tsx
+++ src/app/research/grants/decentraland/page.tsx
@@ -15,18 +15,20 @@ export default function Decentraland() {
   
       
       <section className={styles.section}>
-        <h2>Decentraland Grants Program</h2>
-      
-        <p><strong>Timeframe: </strong>Quarterly cohorts, active year-round.</p>
-        <p><strong>Grant amount:</strong>$20,000 to $240,000 USD</p>
-<p> The Decentraland grant program has funded over 200 projects in the ecosystem. The Decentraland DAO manages the grants program
-</p> 
-<p>Applicants can submit projects under seven categories: Core Unit, Platform, Documentation, In-world Content, Social Media Content, Sponsorship, and Accelerator.</p>
-  </section>
+        <h2 className={styles.decentraland}>Decentraland Grants Program</h2>
+        <img src="/images/decentraland-grant.svg" alt="Decentraland" className={styles.benefitsImage} /> 
+<br></br>
+<br></br>
+<h1> What is the Decentraland Grants Program?
+</h1>
 
+<p>The program is managed by the Decentraland DAO to support projects across seven categories: Core Unit, Platform, Documentation, In-world Content, Social Media Content, Sponsorship, and Accelerator. Since its inception the program has funded over 200 projects in the Decentraland ecosystem.
+</p> 
+  </section>
+<br></br>
  
     <section className={styles.section}>
-   <h1>Grant Scope</h1> 
+   <h1>Scope</h1> 
 <p>The program’s scope covers seven areas of interest:</p>
    
 <ul className={styles.list}>
@@ -39,23 +41,27 @@ export default function Decentraland() {
       <li><strong>Accelerator:</strong> Provides financial support for private companies or for-profit initiatives aligned with Decentraland’s goals and in need of assistance.</li>
 
           </ul> 
-    
-   
-   
-   
-    <h1>Funding Specifics</h1>
-    <p>Projects may request grants within two possible tiers regardless of the grant category.</p>
+  
+   </section>
+   <br></br>
+   <section className={styles.section}> 
+    <h1>Funding</h1>
+    <p>Projects of all categories can apply to two tiers of funding:</p>
     <ul className={styles.list}>
-      <li>The lower tier allows up to $20,000, paid out in stablecoins or MANA using a Vesting Contract as a one-time payment (after 30 days) or with a vesting schedule. </li>
-      <li>The higher tier allows grants from $20,000 to $240,000, paid out in stablecoins using a Vesting Contract.</li>
+      <li><strong>Lower tier:</strong> allows up to $20,000, paid out in stablecoins or MANA using a Vesting Contract as a one-time payment (after 30 days) or with a vesting schedule. </li>
+      <li><strong>Higher tier:</strong> allows grants from $20,000 to $240,000, paid out in stablecoins using a Vesting Contract.</li>
 
     </ul> 
-    <p>The DAO community must approve grant proposals.</p>
+    <p>The DAO community must approve all grant proposal amounts.</p>
 </section>
 
+
+<br></br>
+
+<section className={styles.section}> 
 <h1>Requirements</h1>
 <p>Grantees are required to follow DAO rules and policies and submit monthly updates on the Governance dApp as per their proposed execution plan.</p>
-<p>Projects are accepted on all stages of development. What matters is the level of detail in each part of the execution.</p>  
+<p>Projects are accepted on all stages of development.</p>  
 
 
 <p>Each grant category has different requirements and performance metrics:</p>
@@ -98,29 +104,40 @@ export default function Decentraland() {
     </ul>
     <p>You can find more information on requirements <a href='https://docs.decentraland.org/player/general/dao/overview/grants-program/#grant-categories-and-requirements' className={styles.a}>here.</a></p>
 
-     
+    </section>
+
+<br></br>
+
+
+
+<section className={styles.section}> 
 <h1>Grant Process</h1>
-<img src="/images/decentraland-timeline.jpg" alt="Decentraland Image 1" className={styles.benefitsImage} />
-<p className={styles.black}>Application</p>
+<img src="/images/decentraland-timelime.svg" alt="Decentraland" className={styles.benefitsImage} />
+
+
+<p className={styles.subtitle}><strong>Application</strong></p>
       <ul className={styles.list}>
-      <li>To request a grant from the Decentraland DAO begin by navigating to <a href='https://decentraland.org/governance/submit/grant/'>decentraland.org/governance/submit/grant/</a> and connect your wallet.</li>
-     <li>Fill out the Grant Proposal Form including category, desired funding, and general information. Here you can find an overview of the form.</li>
+      <p>1. To request a grant from the Decentraland DAO begin by navigating to <a href='https://decentraland.org/governance/submit/grant/' className={styles.a}>decentraland.org/governance/submit/grant/</a> and connect your wallet.</p>
+     <p>2. Fill out the Grant Proposal Form including category, desired funding, and general information.<a href='https://docs.decentraland.org/player/general/dao/overview/requesting-grants/' className={styles.a} target='_blank'> Here</a> you can find an overview of the form.</p>
     </ul>
 
-    <p className={styles.black}>Review</p>
+    <p className={styles.subtitle}><strong>Review</strong></p>
       <ul className={styles.list}>
       <li>After submitting your proposal, a Forum thread will be generated automatically where the community can provide feedback or ask questions about your project.</li>
      <li>The DAO will select projects through a voting process. Grant requests remain for 14 days.</li>
-     <li>If selected the project must comply with monthly reports and communicate openly with the community. For more information on actions after being selected to receive a grant and how grantees are expected to report navigate<a className={styles.a} href='https://docs.decentraland.org/player/general/dao/overview/receiving-grants/'> here </a></li>
+     <li>If selected the project must comply with monthly reports and communicate openly with the community. For more information on actions after being selected to receive a grant and how grantees are expected to report navigate<a className={styles.a} href='https://docs.decentraland.org/player/general/dao/overview/receiving-grants/' target='_blank'> here </a></li>
     </ul>
+<br></br>
 
-    <h1>Review</h1>
+
+    <h1>Rosources</h1>
       <ul className={styles.list}>
      <li><a className={styles.a} href='https://docs.decentraland.org/player/general/dao/overview/grants-faq/'>Decentraland Grants FAQ</a></li>
+  <li><a className={styles.a} href='https://docs.decentraland.org/player/general/dao/overview/requesting-grants/'>Grant submission form overview</a></li>
    <li><a className={styles.a} href='https://decentraland.org/governance/projects/?type=grants'>Approved Projects</a></li>
    
-   </ul> 
-
+  </ul> 
+  </section>
      <SideIcons />
      <BackToTop />
     </div>
