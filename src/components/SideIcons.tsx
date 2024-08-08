18ff806a87fb7123f3f12b477bb18a54ffa53a60
Javier Oviedo <javieroviedo@192.168.1.17>
2024-08-08 19:35:01 -0300

    changes

--- src/components/SideIcons.tsx
+++ src/components/SideIcons.tsx
@@ -1,12 +1,44 @@
 // src/components/SideIcons.tsx
 import React from 'react';
 import styles from './SideIcons.module.css';
+import jsPDF from 'jspdf';
 
+import html2canvas from 'html2canvas';
+
+const downloadPDF = () => {
+  const input = document.body; // O especifica el selector de la parte de la página que deseas capturar
+
+  html2canvas(input)
+    .then((canvas) => {
+      const imgData = canvas.toDataURL('image/png');
+      const pdf = new jsPDF();
+      const imgWidth = 210; // Ancho A4 en mm
+      const pageHeight = 295; // Alto A4 en mm
+      const imgHeight = (canvas.height * imgWidth) / canvas.width;
+      let heightLeft = imgHeight;
+      let position = 0;
+
+      pdf.addImage(imgData, 'PNG', 0, position, imgWidth, imgHeight);
+      heightLeft -= pageHeight;
+
+      while (heightLeft >= 0) {
+        position = heightLeft - imgHeight;
+        pdf.addPage();
+        pdf.addImage(imgData, 'PNG', 0, position, imgWidth, imgHeight);
+        heightLeft -= pageHeight;
+      }
+
+      pdf.save('page.pdf');
+    })
+    .catch((error) => {
+      console.error('Error generating PDF:', error);
+    });
+};
 const SideIcons: React.FC = () => {
     return (
         <div className={styles.sideIcons}>
             <div className={styles.iconContainer}>
-            <a href="" download>
+            <a onClick={downloadPDF}>
                 <img src="/icons/guardar-article.svg" alt="Back to top" className={styles.icon} onClick={() => window.scrollTo({ top: 0, behavior: 'smooth' })} />
           </a>
             </div>
