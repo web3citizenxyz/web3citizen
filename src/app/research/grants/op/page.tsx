746e4a57e9b022b9779277764859e5f306cc412e
Javier Oviedo <javieroviedo@192.168.1.17>
2024-08-08 10:35:41 -0300

    op funding

--- src/app/research/grants/op/page.tsx
+++ src/app/research/grants/op/page.tsx
@@ -3,6 +3,7 @@ import React from 'react';
 import styles from '../DaoBasics.module.css';
 import SideIcons from '@/components/SideIcons';
 import BackToTop from '@/components/BackToTop';
+import { StyleRegistry } from 'styled-jsx';
 
 
 
@@ -15,17 +16,27 @@ export default function DaoBasics() {
   
       
       <section className={styles.section}>
-        <h2>Foundation Missions (RFPs)</h2>
-       
-        <p>Foundation Missions are proposals for specific initiatives that achieve a Collective Intent.</p>
-        <p>Foundation Missions act as requests for proposals (RFPs) that teams can apply for. Missions also typically come with specified grant amounts for their completion.</p>
-    <p>The idea is simple: you craft what the Foundation has outlined and, in return, you are compensated based on the perceived effort it entails. It's an excellent opportunity, especially if you're keen to contribute but don't have a personal project in the pipeline.</p>
+        <h2 className={styles.op}>Optimism Foundation Missions</h2>
+        <br></br>
+        <img src="/images/foundation-missions.svg" alt="OP Foundation Missions" className={styles.benefitsImage} /> 
+<br></br>
+       <br></br>
+         <h1 className={styles.subtitle}>What is a Foundation Mission?</h1>
+
+        <p>In Optimism, Foundation Missions are proposals for specific initiatives that achieve a Collective Intent.</p>
+        <p>They act as requests for proposals (RFPs) that teams can apply for throughout the season. Missions also typically come with specified grant amounts for their completion.</p>
+        <p>The idea is simple: you craft what the Foundation has outlined and, in return, you are compensated based on the perceived effort it entails. It's an excellent opportunity if you're keen to contribute but don't have a personal project in the pipeline.</p>
+<br></br>
+  
+  
+  <h1>Scope</h1>
    <p>Grant scope varies! You can view the full list of Foundation Missions (RFPs) <a className={styles.a}> here.</a></p>
       </section>
 
+
       <section className={styles.section}>
 
-      <h1>Grant Requirements</h1>  
+      <h1> Requirements</h1>  
    
    <ul className={styles.list}>
      <li>Make sure you qualify for the Trust Tier you are applying for. You can see this within each RFP. </li>
@@ -33,7 +44,7 @@ export default function DaoBasics() {
  
    </ul>
 
-
+ <br></br>
 
 
 
@@ -41,7 +52,7 @@ export default function DaoBasics() {
        <div>
 
       <ol className={styles.list}>
-        <li> Find an open Foundation Mission that you’d like to work on.</li>
+        <li> Find an <a href='https://github.com/ethereum-optimism/ecosystem-contributions/issues?q=is%3Aissue+is%3Aopen+label%3A%22Foundation+Mission+Request%22' className={styles.a} target='_blank'> open Foundation Mission</a> that you’d like to work on.</li>
         <li>Submit your application by replying as a comment on the RFP’s Github Issue.</li>
         <li>Foundation Mission Requests will be evaluated and selected by the Optimism Foundation.</li>
     
@@ -53,13 +64,9 @@ export default function DaoBasics() {
 
       </section>
 
-
-      <section className={styles.section}>
-
-  
-     
-
-<h1>Resources</h1> 
+  <br></br>
+   <section className={styles.section}>
+    <h1>Resources</h1> 
    <ul className={styles.list}>
     <li> <a className={styles.a} href="https://www.loom.com/share/c7f43e6ab8a44005969af46b7fd7a124" target="_blank" rel="noopener noreferrer">Loom tutorial</a> </li>
  
